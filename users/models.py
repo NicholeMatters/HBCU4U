68f37caaa55c320990a49c80a6fcc048cbf2c8e7
hexagonstem <bishara@hexagon-stem.com>
2020-08-04 17:26:11 +0000

    create users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Consider creating a custom user model from scratch as detailed at
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#specifying-a-custom-user-model
+
+
+class User(AbstractUser):
+    pass
