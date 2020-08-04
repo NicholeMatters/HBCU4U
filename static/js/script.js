5c3f607f33d55be5020b07424a083f6a3a3617b4
Nichole Ross <thisnicholematters@gmail.com>
2020-08-04 22:01:10 +0000

    carousel working.. working on search bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,26 @@
 //Homepage
 
 // Initialize all elements with carousel class.
-  <script>
-a = 0;
-var move = 255;
-var slideNum = document.getElementsByClassName("item").length;
-
-left_button.onclick = function () {
-    a = (a - 1) % slideNum; // Move back a slide   
-    if( a == -1 ) a = slideNum - 1; // If we move past the first slide, move to the last
-    ruller.style.left = (a*move) + 'px';
-    console.log(a*move); // Consoles the value we want 
-}
-
-right_button.onclick = function () {
-    a = (a + 1) % slideNum; // Moves to the next slide, will move to the first when need due to %
-    ruller.style.left = (a*move) + 'px';
-    console.log(a*move); 
-}
-    </script>
-
+$(document).ready(function(){
+			$('.customer-logos').slick({
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				autoplay: true,
+				autoplaySpeed: 1000,
+				arrows: false,
+				dots: false,
+					pauseOnHover: false,
+					responsive: [{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 3
+					}
+				}, {
+					breakpoint: 520,
+					settings: {
+						slidesToShow: 2
+					}
+				}]
+			});
+		});
     
