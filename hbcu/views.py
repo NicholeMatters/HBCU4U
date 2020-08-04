883716617fc3b3b22ca5f626569354949e65357e
hexagonstem <bishara@hexagon-stem.com>
2020-08-04 12:21:58 +0000

    a signup button on index.html and a signup form with fields for Username and password with Password confirmation.

--- hbcu/views.py
+++ hbcu/views.py
@@ -35,4 +35,4 @@ def signup(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'hbcu/signup.html', {'form': form})
