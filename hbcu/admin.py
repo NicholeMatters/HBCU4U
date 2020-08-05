6b231d25e346e8504fe5662fbcf9a5ed99e0653c
hexagonstem <bishara@hexagon-stem.com>
2020-08-05 08:38:32 +0000

    Django Map Widgets

--- hbcu/admin.py
+++ hbcu/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import College
 
+from django.contrib.gis.db import models
+from mapwidgets.widgets import GooglePointFieldWidget
+
+
+
 # Register your models here.
 admin.site.register(College)
+
+class CityAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.PointField: {"widget": GooglePointFieldWidget}
+    }
--- hbcu/forms.py
+++ hbcu/forms.py
@@ -3,6 +3,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
+from mapwidgets.widgets import GooglePointFieldWidget, GoogleStaticOverlayMapWidget
+
+
+
 CustomUser = get_user_model()
 
 class CustomUserCreationForm(UserCreationForm):
@@ -11,3 +15,12 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'birth_date', 'password1', 'password2', )
+
+class CityForm(forms.ModelForm):
+
+    class Meta:
+        model = City
+        fields = ("coordinates", "city_hall")
+        widgets = {
+            'coordinates': GooglePointFieldWidget,
+            'city_hall': GoogleStaticOverlayMapWidget,
