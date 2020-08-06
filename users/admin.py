05a985e5f0e468f4807e5dfc2cf1f14ccc6b537b
hexagonstem <bishara@hexagon-stem.com>
2020-08-06 03:11:48 +0000

    converted all files and folders

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,5 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
 
 admin.site.register(User, UserAdmin)
