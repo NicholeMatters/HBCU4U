a348e268015449dd80fa9dfe5e062451e1763000
Nichole Ross <thisnicholematters@gmail.com>
2020-08-11 20:02:44 +0000

    add map

--- hbcu/views.py
+++ hbcu/views.py
@@ -8,12 +8,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 # Filter Imports
-from django.db.models import Q, Count
-# from django.shortcuts import render, get_object_or_404
-from rest_framework import generics
-from rest_framework.response import Response
-from .models import Journal, Category
-from .serializers import JournalSerializer
+# from django.db.models import Q, Count
+# # from django.shortcuts import render, get_object_or_404
+# from rest_framework import generics
+# from rest_framework.response import Response
+# from .models import Journal, Category
+# from .serializers import JournalSerializer
 
 # Create your views here.
 def index(request):
@@ -62,6 +62,11 @@ def add_hbcu(request):
 
     return render(request, "hbcu/add_hbcu.html", {"form": form})
 
+# Adding hbcumap.html
+def map(request):
+    return render(request, 'hbcu/hbcumap.html', {})
+
+
 # User login
 @login_required
 def home(request):
@@ -86,96 +91,96 @@ def is_valid_queryparam(param):
     return param != '' and param is not None
 
 
-def filterB(request):
-    qs = Journal.objects.all()
-    categories = Category.objects.all()
-    title_contains_query = request.GET.get('title_contains')
-    id_exact_query = request.GET.get('id_exact')
-    title_or_author_query = request.GET.get('title_or_author')
-    view_count_min = request.GET.get('view_count_min')
-    view_count_max = request.GET.get('view_count_max')
-    date_min = request.GET.get('date_min')
-    date_max = request.GET.get('date_max')
-    category = request.GET.get('category')
-    reviewed = request.GET.get('reviewed')
-    not_reviewed = request.GET.get('notReviewed')
+# def filterB(request):
+#     qs = Journal.objects.all()
+#     categories = Category.objects.all()
+#     title_contains_query = request.GET.get('title_contains')
+#     id_exact_query = request.GET.get('id_exact')
+#     title_or_author_query = request.GET.get('title_or_author')
+#     view_count_min = request.GET.get('view_count_min')
+#     view_count_max = request.GET.get('view_count_max')
+#     date_min = request.GET.get('date_min')
+#     date_max = request.GET.get('date_max')
+#     category = request.GET.get('category')
+#     reviewed = request.GET.get('reviewed')
+#     not_reviewed = request.GET.get('notReviewed')
 
-    if is_valid_queryparam(title_contains_query):
-        qs = qs.filterB(title__icontains=title_contains_query)
+#     if is_valid_queryparam(title_contains_query):
+#         qs = qs.filterB(title__icontains=title_contains_query)
 
-    elif is_valid_queryparam(id_exact_query):
-        qs = qs.filterB(id=id_exact_query)
+#     elif is_valid_queryparam(id_exact_query):
+#         qs = qs.filterB(id=id_exact_query)
 
-    elif is_valid_queryparam(title_or_author_query):
-        qs = qs.filterB(Q(title__icontains=title_or_author_query)
-                       | Q(author__name__icontains=title_or_author_query)
-                       ).distinct()
+#     elif is_valid_queryparam(title_or_author_query):
+#         qs = qs.filterB(Q(title__icontains=title_or_author_query)
+#                        | Q(author__name__icontains=title_or_author_query)
+#                        ).distinct()
 
-    if is_valid_queryparam(view_count_min):
-        qs = qs.filterB(views__gte=view_count_min)
+#     if is_valid_queryparam(view_count_min):
+#         qs = qs.filterB(views__gte=view_count_min)
 
-    if is_valid_queryparam(view_count_max):
-        qs = qs.filterB(views__lt=view_count_max)
+#     if is_valid_queryparam(view_count_max):
+#         qs = qs.filterB(views__lt=view_count_max)
 
-    if is_valid_queryparam(date_min):
-        qs = qs.filterB(publish_date__gte=date_min)
+#     if is_valid_queryparam(date_min):
+#         qs = qs.filterB(publish_date__gte=date_min)
 
-    if is_valid_queryparam(date_max):
-        qs = qs.filterB(publish_date__lt=date_max)
+#     if is_valid_queryparam(date_max):
+#         qs = qs.filterB(publish_date__lt=date_max)
 
-    if is_valid_queryparam(category) and category != 'Choose...':
-        qs = qs.filterB(categories__name=category)
+#     if is_valid_queryparam(category) and category != 'Choose...':
+#         qs = qs.filterB(categories__name=category)
 
-    if reviewed == 'on':
-        qs = qs.filterB(reviewed=True)
+#     if reviewed == 'on':
+#         qs = qs.filterB(reviewed=True)
 
-    elif not_reviewed == 'on':
-        qs = qs.filterB(reviewed=False)
+#     elif not_reviewed == 'on':
+#         qs = qs.filterB(reviewed=False)
 
-    return qs
-
-
-def infinite_filter(request):
-    limit = request.GET.get('limit')
-    offset = request.GET.get('offset')
-    return Journal.objects.all()[int(offset): int(offset) + int(limit)]
+#     return qs
 
 
-def is_there_more_data(request):
-    offset = request.GET.get('offset')
-    if int(offset) > Journal.objects.all().count():
-        return False
-    return True
+# def infinite_filter(request):
+#     limit = request.GET.get('limit')
+#     offset = request.GET.get('offset')
+#     return Journal.objects.all()[int(offset): int(offset) + int(limit)]
 
 
-def BootstrapFilterView(request):
-    qs = filterB(request)
-    context = {
-        'queryset': qs,
-        'categories': Category.objects.all()
-    }
-    return render(request, "bootstrap_form.html", context)
+# def is_there_more_data(request):
+#     offset = request.GET.get('offset')
+#     if int(offset) > Journal.objects.all().count():
+#         return False
+#     return True
 
 
-class ReactFilterView(generics.ListAPIView):
-    serializer_class = JournalSerializer
+# def BootstrapFilterView(request):
+#     qs = filterB(request)
+#     context = {
+#         'queryset': qs,
+#         'categories': Category.objects.all()
+#     }
+#     return render(request, "bootstrap_form.html", context)
 
-    def get_queryset(self):
-        qs = filterB(self.request)
-        return qs
+
+# class ReactFilterView(generics.ListAPIView):
+#     serializer_class = JournalSerializer
+
+#     def get_queryset(self):
+#         qs = filterB(self.request)
+#         return qs
 
 
-class ReactInfiniteView(generics.ListAPIView):
-    serializer_class = JournalSerializer
+# class ReactInfiniteView(generics.ListAPIView):
+#     serializer_class = JournalSerializer
 
-    def get_queryset(self):
-        qs = infinite_filter(self.request)
-        return qs
+#     def get_queryset(self):
+#         qs = infinite_filter(self.request)
+#         return qs
 
-    def list(self, request):
-        queryset = self.get_queryset()
-        serializer = self.serializer_class(queryset, many=True)
-        return Response({
-            "journals": serializer.data,
-            "has_more": is_there_more_data(request)
-        })
+#     def list(self, request):
+#         queryset = self.get_queryset()
+#         serializer = self.serializer_class(queryset, many=True)
+#         return Response({
+#             "journals": serializer.data,
+#             "has_more": is_there_more_data(request)
+#         })
