a77aef175f61dfd7f7356d3415f2655c4ed5e0f7
Nichole Ross <thisnicholematters@gmail.com>
2020-08-11 19:05:23 +0000

    filter fields created

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,15 @@
+// FILTER IMPORTS
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import authReducer from "./store/reducers/auth";
+
+
 //Hamburger Menu
 function hiddenMenu() {
   var x = document.getElementById("myLinks");
@@ -165,3 +177,22 @@ function readMore() {
     moreText.style.display = "inline";
   }
 }
+
+// filter
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, composeEnhances(applyMiddleware(thunk)));
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root"));
+registerServiceWorker();
